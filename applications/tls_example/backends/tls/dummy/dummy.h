f1a55902fff94211606dafbdb593650a7db6c450
Yecheng Zhao <zyecheng@google.com>
2021-03-19 14:01:24 -0700

    [tls] Generate size report for tls example
    Generate size report for the tls example application. The report is
    on additional binary size contributed by the use of tls library. This is
    done by building another executable target that uses a noop tls backend,
    then bloating it against the original target.
    
    Change-Id: Ibb4514376c22b18f27b1e910248d46627766402f
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/experimental/+/39462
    Commit-Queue: Yecheng Zhao <zyecheng@google.com>
    Reviewed-by: Ali Zhang <alizhang@google.com>

---
+++ applications/tls_example/backends/tls/dummy/dummy.h
@@ -0,0 +1,29 @@
+// Copyright 2021 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#pragma once
+
+#include "backend_interface.h"
+
+class DummyTls final : public TlsInterface {
+public:
+  DummyTls()  {}
+  const char* Name() override { return ""; }
+  int SetHostName(const char *) override { return 0; }
+  int Handshake(TransportInterface* ) override { return 0; }
+  int Write(const void* , size_t , TransportInterface* ) override { return 0; }
+  int Read(void* , size_t , TransportInterface* ) override { return 0; }
+  int LoadCACert(const void* , size_t ) override { return 0; }
+  int LoadCrl(const void* , size_t ) override { return 0; }
+};
