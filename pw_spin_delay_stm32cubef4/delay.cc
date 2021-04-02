94c09a5fae61a2769d4006d27dd4a5c3d5c6169f
Varun Sharma <vars@google.com>
2021-04-01 21:24:05 -0700

    [stm32] Add pw_spin_delay_stm32cubef4
    Change-Id: I2b2b16047508f4c0bacfe77b31856dd6c9b0362a
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/experimental/+/39768
    Commit-Queue: Varun Sharma <vars@google.com>
    Reviewed-by: Ali Zhang <alizhang@google.com>

---
+++ pw_spin_delay_stm32cubef4/delay.cc
@@ -0,0 +1,25 @@
+// Copyright 2021 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#include "pw_spin_delay/delay.h"
+
+#include "stm32f4xx.h"
+
+namespace pw::spin_delay {
+
+void WaitMillis(size_t delay_ms) { HAL_Delay(delay_ms); }
+
+uint32_t Millis() { return HAL_GetTick(); }
+
+}  // namespace pw::spin_delay
