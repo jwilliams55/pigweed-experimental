552c42950eca992dbf8a9055de7c08eae5b9c93f
Anthony DiGirolamo <tonymd@google.com>
2022-05-18 13:17:20 -0700

    pw_display: New facade definitions
    Introduce new facades for interacting with small screens with
    resistive touch.
    
    - pw_display
    - pw_touchscreen
    
    Change-Id: If90b9edc940cfe92e78a0d28d284bd58744a6875
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/experimental/+/95201
    Reviewed-by: Armando Montanez <amontanez@google.com>
    Commit-Queue: Anthony DiGirolamo <tonymd@google.com>

---
+++ pw_display/public/pw_display/display.h
@@ -0,0 +1,35 @@
+// Copyright 2022 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+#pragma once
+
+#include "pw_coordinates/vec_int.h"
+#include "pw_framebuffer/rgb565.h"
+
+using namespace pw::framebuffer;
+
+namespace pw::display {
+
+void Init();
+uint16_t* GetInternalFramebuffer();
+// TODO(tonymd): Add a DPI or physical size value.
+int GetWidth();
+int GetHeight();
+// TODO(tonymd): Add update functions for new framebuffer types or make this a
+// templated class.
+void Update(FramebufferRgb565* framebuffer);
+bool TouchscreenAvailable();
+bool NewTouchEvent();
+pw::coordinates::Vec3Int GetTouchPoint();
+
+}  // namespace pw::display
