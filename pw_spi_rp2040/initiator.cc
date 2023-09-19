801766df587bf7d6083363b2d57bbd7b868e0db6
Anthony DiGirolamo <tonymd@google.com>
2023-09-19 16:29:26 +0000

    pw_spi_rp2040: Renamed from pw_spi_pico
    Change-Id: I1d8c20e25e5b8d14c508b72e831353585d7aeb7b
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/experimental/+/171702
    Pigweed-Auto-Submit: Anthony DiGirolamo <tonymd@google.com>
    Reviewed-by: Erik Gilling <konkers@google.com>
    Presubmit-Verified: CQ Bot Account <pigweed-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Commit-Queue: Auto-Submit <auto-submit@pigweed-service-accounts.iam.gserviceaccount.com>

--- pw_spi_rp2040/initiator.cc
+++ pw_spi_rp2040/initiator.cc
@@ -12,7 +12,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-#include "pw_spi_pico/initiator.h"
+#include "pw_spi_rp2040/initiator.h"
 
 #include <algorithm>
 
